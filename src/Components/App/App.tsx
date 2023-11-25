4e5f2e633c4c7d71bd46a66f1c984ed556714894
Sergey Berdnikov <sega12b@yandex.ru>
2023-11-25 13:48:42 +0500

    fix: component path for vercel

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,12 +1,11 @@
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { MainPage } from 'pages/MainPage/MainPage';
 
-function App() {
+export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
     </Routes>
   );
-}
-
-export default App;
+};
