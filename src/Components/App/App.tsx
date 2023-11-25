6808a2efee43ca9419bc2f71eb448d950248d346
Sergey Berdnikov <sega12b@yandex.ru>
2023-11-25 13:31:03 +0500

    feat: made Main page base layout

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,5 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
+import { MainPage } from 'pages/MainPage/MainPage';
+
 function App() {
-  return <div>Hello, World</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import './MainPage.scss';
+
+export const MainPage = () => {
+  return (
+    <section className="main-page" aria-label="Главная страница">
+      <header className="main-page__header">Header</header>
+      <main className="main-page__main">Main</main>
+      <footer className="main-page__footer">Prosept&copy;</footer>
+    </section>
+  );
+};
