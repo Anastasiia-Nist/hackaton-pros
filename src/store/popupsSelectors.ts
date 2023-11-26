e68289dc18020f75de58086bc5bfaf328f0d5c6f
Sergey Berdnikov <sega12b@yandex.ru>
2023-11-26 20:12:27 +0500

    feat: made Login popup

--- src/store/popupsSelectors.ts
+++ src/store/popupsSelectors.ts
@@ -3,3 +3,7 @@ import { StateType } from './store';
 export const registerPopupSelector = (state: StateType) => {
   return state.popups.isRegisterPopupOpen;
 };
+
+export const loginPopupSelector = (state: StateType) => {
+  return state.popups.isLoginPopupOpen;
+};
