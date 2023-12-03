0b36d55c62a4140ae45dbaddc84b7b9514eb9ebc
Sergey Berdnikov <sega12b@yandex.ru>
2023-12-03 15:21:56 +0500

    feat: made Statistics page base layout

--- src/pages/StatisticsPage/StatisticsPage.tsx
+++ src/pages/StatisticsPage/StatisticsPage.tsx
@@ -3,7 +3,33 @@ import './StatisticsPage.scss';
 export const StatisticsPage = () => {
   return (
     <section className="statistics-page">
-      <main className="statistics-page__main">StatisticsPage</main>
+      <main className="statistics-page__main">
+        <section>
+          <h2 className="statistics-page__title">Статистика текущего сеанса</h2>
+          <ul className="statistics-page__current-session">
+            <li className="statistics-page__session-item">
+              <h3>Размечено</h3>
+              <p>text</p>
+            </li>
+            <li className="statistics-page__session-item">
+              <h3>Выбирается по счету вариант</h3>
+              <p>text</p>
+            </li>
+            <li className="statistics-page__session-item">
+              <h3>Удачных предсказаний</h3>
+              <p className="statistics-page__session-item-success">text</p>
+            </li>
+            <li className="statistics-page__session-item">
+              <h3>Неудачных предсказаний</h3>
+              <p className="statistics-page__session-item-failed">text</p>
+            </li>
+            <li className="statistics-page__session-item">
+              <h3>Отложенных</h3>
+              <p>text</p>
+            </li>
+          </ul>
+        </section>
+      </main>
     </section>
   );
 };
